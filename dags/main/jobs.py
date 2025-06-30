de75ff04571dc151d696ad3a64a54a78afae4f83
Nam Tonthat <n.nam.tonthat@gmail.com>
2025-06-30 22:08:37 +1000

    feat: remove main dag

--- dags/main/jobs.py
+++
@@ -1,7 +0,0 @@
-from dagster import AssetSelection, define_asset_job
-
-sample_job = define_asset_job(
-    name="sample_job",
-    selection=AssetSelection.all(),
-    description="Sample job that materializes all assets",
-)
