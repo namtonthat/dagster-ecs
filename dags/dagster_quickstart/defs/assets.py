4c969b37d4db456eb752b564144fc75a0b998271
Nam Tonthat <n.nam.tonthat@gmail.com>
2025-06-30 22:15:45 +1000

    fix: last dags

--- dags/dagster_quickstart/defs/assets.py
+++ dags/dagster_quickstart/defs/assets.py
@@ -11,9 +11,7 @@ def processed_data():
     df = pd.read_csv(sample_data_file)
 
     ## Add an age_group column based on the value of age
-    df["age_group"] = pd.cut(
-        df["age"], bins=[0, 30, 40, 100], labels=["Young", "Middle", "Senior"]
-    )
+    df["age_group"] = pd.cut(df["age"], bins=[0, 30, 40, 100], labels=["Young", "Middle", "Senior"])
 
     ## Save processed data
     df.to_csv(processed_data_file, index=False)
